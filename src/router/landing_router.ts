15a9fe0185b7db0dd4b53c8dc59078bd4397f0b3
TheElegantCoding <luismonsalve1941@gmail.com>
2026-04-29 23:28:20 -0400

    feat: add landing router for multilingual support

---
+++ src/router/landing_router.ts
@@ -0,0 +1,16 @@
+const landingRouter = {
+  es: {
+    principal: '/es',
+    aboutUs: '/es/sobre-nosotros'
+  },
+  en: {
+    principal: '/en',
+    aboutUs: '/en/about-us'
+  },
+  fr: {
+    principal: '/fr',
+    aboutUs: '/fr/a-propos-de-nous'
+  }
+};
+
+export { landingRouter };
