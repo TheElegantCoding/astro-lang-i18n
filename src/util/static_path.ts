25f41079faaa1157b89915f3a0de91adcf36a500
TheElegantCoding <luismonsalve1941@gmail.com>
2026-05-07 18:14:31 -0400

    refactor: simplify getStaticLanguage function and remove unused getDynamicLangRoute

--- src/util/static_path.ts
+++ src/util/static_path.ts
@@ -1,41 +1,11 @@
-/* eslint-disable max-statements */
-import { removeLanguageFromUrl } from 'src/util/i18n';
+const getStaticLanguage = (supportedLanguages: string[], parameter = 'lang') => {
+  const language: { params: Record<string, string> }[] = [];
 
-type GetDynamicLangRouteParameter = {
-  defaultLang?: string;
-  excludeDefaultLang?: boolean;
-  router: Record<string, unknown>;
-  mappings?: Record<string, unknown>;
+  supportedLanguages.forEach((lang) => {
+    return language.push({ params: { [parameter]: lang } });
+  });
+
+  return language;
 };
 
-const getDynamicLangRoute = ({
-  router,
-  mappings = {},
-  excludeDefaultLang = false,
-  defaultLang
-}: GetDynamicLangRouteParameter) => {
-  return Object.entries(router).map(([lang, pages]) => {
-    if (excludeDefaultLang && lang === defaultLang) { return null; }
-
-    const pagesForLang = pages as Record<string, string>;
-    const parameters: Record<string, string> = { lang };
-
-    const firstKey = Object.values(mappings)[0] as string;
-    const fullPath = pagesForLang[firstKey] as string | undefined;
-
-    if (!fullPath) { return null; }
-
-    const segments = removeLanguageFromUrl(fullPath, lang).split('/').filter(Boolean);
-
-    Object.keys(mappings).forEach((fileParameter, index) => {
-      if (segments[index]) {
-        parameters[fileParameter] = segments[index];
-      }
-    });
-
-    return { params: parameters };
-  }).filter((path) => { return path !== null; });
-};
-
-export type { GetDynamicLangRouteParameter };
-export { getDynamicLangRoute };
+export { getStaticLanguage };
