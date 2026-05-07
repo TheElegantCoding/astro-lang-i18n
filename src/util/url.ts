b9fe5164c3db06f659b4d7b3a923c34d65e6d460
TheElegantCoding <luismonsalve1941@gmail.com>
2026-05-06 21:39:48 -0400

    refactor: remove unused buildUrl function from url utility

--- src/util/url.ts
+++ src/util/url.ts
@@ -2,18 +2,4 @@ const removeTrailingSlash = (url: URL | string) => {
   return url.toString().replace(/\/$/v, '');
 };
 
-const buildUrl = (path: string, base?: URL | string): string => {
-  if (!base) {
-    return removeTrailingSlash(path);
-  }
-
-  if (path.startsWith('http://') || path.startsWith('https://')) {
-    return removeTrailingSlash(path);
-  }
-
-  const baseUrl = removeTrailingSlash(new URL(base));
-  const cleanPath = path.replace(/^\//, '');
-  return removeTrailingSlash(`${baseUrl}/${cleanPath}`);
-};
-
-export { buildUrl, removeTrailingSlash };
+export { removeTrailingSlash };
