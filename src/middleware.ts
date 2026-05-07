f62daf56297063703c34bb50158cee81f2dbfab5
TheElegantCoding <luismonsalve1941@gmail.com>
2026-05-07 16:50:53 -0400

    refactor: simplify i18n middleware setup by removing defineMiddleware and streamlining initialization

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,9 @@
-import { sequence, defineMiddleware } from 'astro:middleware';
+import { sequence } from 'astro:middleware';
 import { defaultLanguage, supportedLanguages } from 'src/configuration/language';
-import { i18nMiddleware } from 'src/util/i18n_middleware';
+import { i18nSetup } from 'src/util/i18n_middleware';
 
-const i18nSetup = defineMiddleware(async (context, next) => {
-  return i18nMiddleware({
-    context,
-    next,
-    defaultLanguage,
-    supportedLanguages
-  });
-});
+const i18n = i18nSetup({ defaultLanguage, supportedLanguages });
 
-const onRequest = sequence(i18nSetup);
+const onRequest = sequence(i18n);
 
 export { onRequest };
