88fd04bf9bc3ca95dda4753552fadae8e7d3edc6
TheElegantCoding <luismonsalve1941@gmail.com>
2026-05-06 21:38:31 -0400

    refactor: remove unused Base component export from index

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,6 @@ import {
 } from './util/i18n';
 import { interpolate } from './util/interpolate';
 
-export { default as Base } from './component/internationalization.astro';
-
 export {
   getI18n,
   interpolate,
